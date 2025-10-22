3a8d677ad4ae3112c6b1ef139675b37a3a77827b
Antonius Riki Hermawan <rikihermawan9342@gmail.com>
2025-10-22 12:50:43 +0700

    Interaksi Pengguna dengan Django Forms & Create View

---
+++ warga/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import Warga, Pengaduan
+
+class WargaForm(forms.ModelForm):
+    class Meta:
+        model = Warga
+        fields = ['nik', 'nama_lengkap', 'alamat', 'nomor_telepon']
+        widgets = {
+            'nik': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Masukkan NIK'}),
+            'nama_lengkap': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Masukkan nama lengkap'}),
+            'alamat': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Masukkan alamat'}),
+            'nomor_telepon': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Masukkan nomor telepon'}),
+        }
+
+class PengaduanForm(forms.ModelForm):
+    class Meta:
+        model = Pengaduan
+        fields = ['pelapor', 'judul', 'deskripsi']
+        
+        # Widget ini agar form terlihat bagus dengan Bootstrap
+        widgets = {
+            'pelapor': forms.Select(attrs={'class': 'form-select'}),
+            'judul': forms.TextInput(attrs={'class': 'form-control'}),
+            'deskripsi': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Membuat field <select> pelapor terurut berdasarkan nama
+        self.fields['pelapor'].queryset = Warga.objects.order_by('nama_lengkap')
--- warga/views.py
+++ warga/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (ListView, DetailView, CreateView)
 from .models import Warga, Pengaduan
+from .forms import WargaForm, PengaduanForm
 
 class WargaListView(ListView):
     model = Warga
+    template_name = 'warga/warga_list.html'  # opsional
 
-class PengaduanListView(ListView):  
+class PengaduanListView(ListView):
     model = Pengaduan
     template_name = 'warga/pengaduan_list.html'
 
 class WargaDetailView(DetailView):
     model = Warga
     template_name = 'warga/warga_detail.html'
+
+class WargaCreateView(CreateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga:warga-list')  # Arahkan ke daftar pengaduan setelah berhasil menambah warga
+
+class PengaduanCreateView(CreateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    
+    # 3. Tentukan ke mana harus pergi setelah form berhasil disimpan
+    success_url = reverse_lazy('warga:pengaduan_list')

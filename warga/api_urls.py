eb263d73bde870ca6a4d7e7e29f9e3cb52a94f28
antoniusrikihermawan <rikihermawan9342@gmail.com>
2025-11-20 09:06:52 +0700

    API Interaktif Penuh dengan ViewSet & Routers

--- warga/api_urls.py
+++ warga/api_urls.py
@@ -1,9 +1,16 @@
 # warga/api_urls.py
 from django.urls import path, include
-from .views import WargaListAPIView, WargaDetailAPIView, PengaduanListAPIView
+# from .views import WargaListAPIView, WargaDetailAPIView, PengaduanListAPIView
+from .views import WargaViewSet, PengaduanViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'warga', WargaViewSet, basename='warga')
+router.register(r'pengaduan', PengaduanViewSet, basename='aduan')
 
 urlpatterns = [
-    path('warga/', WargaListAPIView.as_view(), name='api-warga-list'),
-    path('warga/<int:pk>/', WargaDetailAPIView.as_view(), name='api-warga-detail'),
-    path('pengaduan/<int:pk>/', PengaduanListAPIView.as_view(), name='api-warga-detail'),
-]
+#     path('warga/', WargaListAPIView.as_view(), name='api-warga-list'),
+#     path('warga/<int:pk>/', WargaDetailAPIView.as_view(), name='api-warga-detail'),
+#     path('pengaduan/<int:pk>/', PengaduanListAPIView.as_view(), name='api-warga-detail'),
+      path('', include(router.urls)),
+ ]
--- warga/views.py
+++ warga/views.py
@@ -3,22 +3,31 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Warga, Pengaduan
 from .forms import WargaForm, PengaduanForm
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+# from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .serializers import WargaSerializer, PengaduanSerializer
+from rest_framework import viewsets
 
+#  --- API VIEWS ---
+class WargaViewSet(viewsets.ModelViewSet):
+     queryset = Warga.objects.all().order_by('-tanggal_registrasi')
+     serializer_class = WargaSerializer
 
-# --- API VIEWS ---
-class WargaListAPIView(ListAPIView):
-    queryset = Warga.objects.all()
-    serializer_class = WargaSerializer
+class PengaduanViewSet(viewsets.ModelViewSet):
+     queryset = Pengaduan.objects.all()
+     serializer_class = PengaduanSerializer
+     
+#  --- API VIEWS ---
+# class WargaListAPIView(ListAPIView):
+#     queryset = Warga.objects.all()
+#     serializer_class = WargaSerializer
 
-class WargaDetailAPIView(RetrieveAPIView):
-    queryset = Warga.objects.all()
-    serializer_class = WargaSerializer
+# class WargaDetailAPIView(RetrieveAPIView):
+#     queryset = Warga.objects.all()
+#     serializer_class = WargaSerializer
 
-class PengaduanListAPIView(ListAPIView):
-    queryset = Pengaduan.objects.all()
-    serializer_class = PengaduanSerializer
+# class PengaduanListAPIView(ListAPIView):
+#     queryset = Pengaduan.objects.all()
+#     serializer_class = PengaduanSerializer
 
 class WargaListView(ListView):
     model = Warga
